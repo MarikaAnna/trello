1b643af6524ed126d341e39688407af7f9efee39
Marika Wojtuń <marika.anna.skrzypczak@gmail.com>
2019-07-21 16:52:29 +0200

    delete test tests

--- pageObjects/DevDiscussTagPage.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-
-export default class DevDiscussTagPage {
-
-    constructor() {
-        this.discussTitle = Selector('h1').withText('Discussion');
-    }
-
-}
